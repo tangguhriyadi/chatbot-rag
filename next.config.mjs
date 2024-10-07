c53ecd8380867bee2065fb557ce93347b54c75f4
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 20:07:06 +0700

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve("./src");
-    return config;
-  },
 };
 
 export default nextConfig;
