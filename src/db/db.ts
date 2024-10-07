1ccc9bee618f31721b3326dcc27fe859b88a89cd
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 20:33:18 +0700

    fix config

--- src/db/db.ts
+++ src/db/db.ts
@@ -21,7 +21,7 @@ export const config = {
     postgresConnectionOptions: {
         type: "postgres",
         host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
+        port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
