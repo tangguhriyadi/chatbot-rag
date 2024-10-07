0759af1a40bf7a4d5b9dfff82573101b16d6fbc8
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 22:03:33 +0700

    fix config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,25 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles/global.css";
 import Providers from "./providers";
 import MainLayout from "../component/layout/main-layout";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
-    title: "Chat RKD",
-    description: "Rekadia Chatbot",
+  title: "Chat RKD",
+  description: "Rekadia Chatbot",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <Providers>
-                    <MainLayout>{children}</MainLayout>
-                </Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          <MainLayout>{children}</MainLayout>
+        </Providers>
+      </body>
+    </html>
+  );
 }
