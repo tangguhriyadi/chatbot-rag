864e6e5594f26cc04f2107179920b24dd610ef05
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 22:10:39 +0700

    fix config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/Component/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./src/component/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./src/Component/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{html,js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
   theme: {
