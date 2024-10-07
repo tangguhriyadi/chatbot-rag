37a58e912ad9fe80ff8c08c5607060f0f596a643
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 19:24:44 +0700

    fxi config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,8 @@ const config = {
         extend: {
             colors: {},
             borderRadius: {},
-            animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
-            },
         },
     },
-    plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
