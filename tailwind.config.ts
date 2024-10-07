b78df6f981a339793b2c91e92e5cbe351445a474
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 21:41:42 +0700

    fix config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Component/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
