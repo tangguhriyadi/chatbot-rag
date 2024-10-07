997895993ecaebf5496f1ac23dc7a0a6782be090
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 20:40:14 +0700

    fix config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
 import type { Config } from "tailwindcss";
 
 const config = {
-    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-    theme: {
-        extend: {
-            colors: {},
-            borderRadius: {},
-        },
+  content: ["./src/app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  prefix: "",
+  theme: {
+    extend: {
+      colors: {},
+      borderRadius: {},
     },
+  },
 } satisfies Config;
 
 export default config;
