62fd8550e002761cab7de45f09dba294bf82067d
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 21:12:00 +0700

    fix config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config = {
-  content: ["./src/app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     extend: {
