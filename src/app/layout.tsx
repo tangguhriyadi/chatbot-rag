9c745c605544c889ebbb4c3ae79b6704ef52608f
tangguhriyadi <mtangguh97@gmail.com>
2024-10-07 20:29:57 +0700

    fix config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-import MainLayout from "../components/layout/main-layout";
+import MainLayout from "../component/layout/main-layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
