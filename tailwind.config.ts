e28e4fa0386ec9a7ab8a2f8f52a83058ed76389b
tangguhriyadi <mtangguh97@gmail.com>
2024-10-08 09:29:00 +0700

    fix config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config = {
-  content: [
-    "./src/pages/**/*.{html,js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{html,js,ts,jsx,tsx,mdx}",
-    "./src/component/**/*.{html,js,ts,jsx,tsx,mdx}",
-    "./src/Components/**/*.{html,js,ts,jsx,tsx,mdx}",
-    "./src/Component/**/*.{html,js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{html,js,ts,jsx,tsx,mdx}",
-    "./src/features/**/*.{html,js,ts,jsx,tsx,mdx}",
-  ],
-  prefix: "",
+  content: ["./src/**/*.{html,js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {},
